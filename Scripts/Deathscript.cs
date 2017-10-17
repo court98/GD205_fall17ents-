8f0f226c164fd478859a61f7cc68a79cfd9c25a5
courtney Ramos <Courtneyramos65@gmail.com>
2017-10-17 07:37:26 -0400

    containsAllmyHomeworkAssigments
    contains Inside Coding Exercise1,1.5,2 and 3

---
+++ Scripts/Deathscript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathscript : MonoBehaviour {
+
+    private GameObject bomb;
+    public Vector3 Checkpoint;
+    public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+        Checkpoint = new Vector3(-7, 0, -8);
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag== ("Player")) 
+
+        {
+            Player.transform.position = Checkpoint=new Vector3(-7, 1, -8);
+        }
+   
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Scripts/Destroycollide.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroycollide : MonoBehaviour {
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+
+
+    }
+
+
+
+    //OnCollisionEnter will run each time the GameObject this is attached to collides with something
+
+    void OnCollisionEnter(Collision collisionReport)
+    {
+
+        Destroy(collisionReport.gameObject); //destroy the gameobject that was collided with as stored in the collisionreport
+
+    }
+
+}
