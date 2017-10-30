5eb11db0c4b5bc9b17d51e928d53e2b40bc2b992
Sinzephy <courtneyramos65@gmail.com>
2017-10-30 19:45:01 -0400

    my carnival Game Ex3
    if you click on the destroyme objects baseballs will spawn from the public transform postion but the objects will not break

---
+++ CarnivalGame/Assets/Scripts/CollideAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideAction : MonoBehaviour {
+	AudioSource myAudio;
+	public AudioClip boomSound;
+	public AudioClip sphereCollide;
+
+	// Use this for initialization
+	void Start () {
+		myAudio = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collisionReport){
+		Debug.Log ("ya done collid'd");
+		myAudio.PlayOneShot(boomSound, 0.7F); //play the boom sound upon any collision
+
+		if (collisionReport.gameObject.tag == "DestroyMe") { //check the collisionReport to see if the GameObject collided with was tagged with "spherez"
+			myAudio.PlayOneShot(sphereCollide, 0.7F); //if so, play the sphereCollide audioclip
+		}
+	}
+}
---
+++ CarnivalGame/Assets/Scripts/Spawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+	public GameObject prefab; //selected prefab from our project view 
+	public Transform spawnPoint; //use the position of this transform as a spawn point
+	public GameObject destroyMe; //destroy this object upon right click
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) { //if the left mouse button is being pressed during this frame...
+			Instantiate (prefab, spawnPoint.position, Quaternion.identity); //instantiate(spawn) our prefab at spawnPoint.position with a rotation of Quaternion.identity("no rotation")
+		}
+
+
+		if (Input.GetMouseButtonDown (1)) { //if the right mouse button has been pressed DOWN during this frame...
+			Destroy (destroyMe); //destroy the destroyMe object -- this will give you an error if you press more than once
+		}		
+	}
+}
