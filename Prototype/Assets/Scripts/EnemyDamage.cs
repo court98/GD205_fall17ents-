b54fc6eb87a36df4dc4720deb7af2a9679b94424
Courtney Ramos <courtneyramos65@gmail.com>
2017-12-18 19:18:25 -0500

    New Scene
    GameOverScene Works

--- Prototype/Assets/Scripts/EnemyDamage.cs
+++ Prototype/Assets/Scripts/EnemyDamage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour {
-
+	
 	int EnemyHealth =100;
 	int TheDamage = 10;
 
@@ -24,4 +24,6 @@ public class EnemyDamage : MonoBehaviour {
 		Destroy(gameObject);
 
 	}
+
 }
+
---
+++ Prototype/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+	public AudioSource GunShot;
+	// Use this for initialization
+	void Start () {
+		GunShot = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+
+			GunShot.Play ();
+		}
+	}
+}
